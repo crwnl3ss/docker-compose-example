071299ed8eaf174a4ccf29241f685c93d0f16c9e
Katsaman Sergey <metalgearangel@gmail.com>
2020-11-06 19:24:22 +0300

    add build time suffix arg

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM golang:1.15.3-alpine3.12
 
+ARG suffix=""
+
 WORKDIR /tmp/
 COPY ./src/main.go /tmp
 RUN CGO_ENABLED=0 go build -o hello_world &&\
   chmod +x hello_world &&\
   cp hello_world /usr/bin/
-ENTRYPOINT [ "hello_world" ]
+
+ENTRYPOINT /usr/bin/hello_world --suffix=${suffix}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+var suffix string
+
+func init() {
+	flag.StringVar(&suffix, "suffix", "", "suffix to print")
+	flag.Parse()
+}
+
 func main() {
 	world := os.Getenv("world")
-	fmt.Printf("hello %s\n", world)
+	fmt.Printf("hello %s %s\n", world, suffix)
 }
